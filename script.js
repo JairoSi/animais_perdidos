8222df1bbea7eea8d8f9311fc8c7a06816daf028
Jairo Si <41476362+JairoSi@users.noreply.github.com>
2025-03-03 14:47:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 // 🔗 Conectar ao Supabase
 const SUPABASE_URL = 'https://itzfzcnpesebessjigkh.supabase.co';
